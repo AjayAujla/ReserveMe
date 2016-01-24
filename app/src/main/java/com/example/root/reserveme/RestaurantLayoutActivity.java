c446b2c8494f4b56f87abf56bbf1dc02fb67e074
Ajayveer Singh Aujla <ajay.aujla@hotmail.com>
2016-01-23 22:21:34 -0500

    Merge conflicts and create button onClick event

--- app/src/main/java/com/example/root/reserveme/RestaurantLayoutActivity.java
+++ app/src/main/java/com/example/root/reserveme/RestaurantLayoutActivity.java
@@ -1,11 +1,9 @@
 package com.example.root.reserveme;
 
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class RestaurantLayoutActivity extends AppCompatActivity {
 
@@ -13,15 +11,10 @@ public class RestaurantLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_layout);
-
-        Button switchToRestaurantLayout = (Button) findViewById(R.id.button_restaurantLayout__viewTables);
-        switchToRestaurantLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(RestaurantLayoutActivity.this, AllTablesActivity.class);
-                RestaurantLayoutActivity.this.startActivity(intent);
-            }
-        });
     }
 
+    public void switchToAllTablesLayout(View view) {
+        Intent intent = new Intent(this, AllTablesActivity.class);
+        this.startActivity(intent);
+    }
 }
